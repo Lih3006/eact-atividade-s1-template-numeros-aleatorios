d9b7b4c12ff343e7282c4d76c555f317ddb00c7e
Lih3006 <alineb3006@hotmail.com>
2023-03-24 10:40:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [number, setNumber] = useState(Math.floor(Math.random() * 100) + 1)
+
+
+  const randomNumber = () => (setNumber(Math.floor(Math.random() * 100) + 1))
+  return (
+    <div >
+      <h1>{number}</h1>
+      <button onClick={randomNumber} >Alterar Numero</button>
+    </div>
+
+  )
+}
+
+export default App
